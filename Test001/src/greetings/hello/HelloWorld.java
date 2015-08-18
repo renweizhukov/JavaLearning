bd755f0894fa241298c0ff4a53162a3b5e8d7cae
renweizhukov <renwei2004@gmail.com>
2015-08-17 17:24:54 -0700

    Java Eclipse Project Test001

---
+++ Test001/src/greetings/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+package greetings.hello;
+
+public class HelloWorld 
+{
+	private static int age = 50;
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world!");
+		
+		System.out.println(age);
+	}
+}
